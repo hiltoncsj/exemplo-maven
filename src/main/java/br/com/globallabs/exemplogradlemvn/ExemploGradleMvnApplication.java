5b306ed4e1482f309b4146b2e4e600618a894f50
hiltoncsj <hiltoncsj@gmail.com>
2021-06-03 02:01:22 -0300

    Adicionado curso de Java DIO com Maven

---
+++ src/main/java/br/com/globallabs/exemplogradlemvn/ExemploGradleMvnApplication.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.exemplogradlemvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradleMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGradleMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/globallabs/exemplogradlemvn/ExemploGradleMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.exemplogradlemvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
